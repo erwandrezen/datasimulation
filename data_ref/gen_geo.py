577ac1a51e001432235520a33470ebb540619f8a
drine29 <63367608+drine29@users.noreply.github.com>
2020-04-09 08:19:16 +0200

    Add files via upload

---
+++ data_ref/gen_geo.py
@@ -0,0 +1,201 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Mar 29 11:26:34 2020
+
+@author: s20kerbr
+"""
+
+# generation automatique de fichiers
+
+import pandas as pd
+
+
+
+header_row = ['nivgeo','codgeo','libgeo','sexe','AGED100' ,'nb']
+header_x = ['codgeo','sexe','AGED100' ,'nb']
+
+header_com=['codgeo','dep','reg','ptot','weight_fr','weight_reg','weight_dep']
+header_com2=['codgeo','dep','reg','ptot']
+
+dtype_com={'codgeo': str}
+
+pop1=pd.read_csv(filepath_or_buffer='F:/PEPS/Nomenclatures/GEO/BTT_TD_POP1B_2016/BTT_TD_POP1B_2016.csv',sep=';', dtype=dtype_com, header=0, names=header_row,usecols=header_x)
+com=pd.read_csv(filepath_or_buffer='F:/PEPS/Données simulées/data_ref/communes_fr.csv',sep=';', dtype=dtype_com, header=0, names=header_com,usecols=header_com2)
+dep=pd.read_csv(filepath_or_buffer='F:/PEPS/Données simulées/data_ref/dep.csv',sep=';')
+ref=pd.read_csv(filepath_or_buffer='F:/PEPS/Données simulées/data_ref/reg.csv',sep=';')
+
+
+
+
+pop1['nb']=pop1['nb'].astype(float)
+pop1['nb_int']=pop1['nb'].astype(int)
+pop1['age']=pop1['AGED100'].astype(int)
+
+
+pop=pd.merge(pop1,com,on='codgeo',how='left')
+
+
+
+# preparation fichier pop men
+# ###########################################################################
+df_men_age1=pop.loc[(pop['sexe']==1), ['codgeo','age' ,'nb_int','dep','reg'] ]
+
+# aggreation par commune (hors age)
+df_men1=df_men_age1.groupby(['codgeo','dep','reg'], as_index=False)["nb_int"].sum()
+# aggreation par age dep reg 
+df_age_m1=df_men_age1.groupby(['age','dep','reg'], as_index=False)["nb_int"].sum()
+# aggreation par age reg 
+df_age_m_reg=df_men_age1.groupby(['age','reg'], as_index=False)["nb_int"].sum()
+# aggreation par hors age
+df_age_m_fr=df_men_age1.groupby(['age'], as_index=False)["nb_int"].sum()
+
+
+# totaux geographique (dep, reg, fr) de la pop men
+dep_men=df_men_age1.groupby("dep", as_index=False)["nb_int"].sum()
+dep_men = dep_men.rename(columns = {'nb_int':'pop_dep'})
+
+reg_men=df_men_age1.groupby("reg", as_index=False)["nb_int"].sum()
+reg_men = reg_men.rename(columns = {'nb_int':'pop_reg'})
+
+# calcul de la proportion de la pop men par dep, reg ou fr
+df_men2=pd.merge(df_men1,dep_men,on='dep',how='left')
+df_men=pd.merge(df_men2,reg_men,on='reg',how='left')
+
+df_men['weight_dep']=(df_men['nb_int']/df_men['pop_dep']*100000)
+df_men['weight_dep']=df_men['weight_dep'].fillna(0.0).astype(int)
+
+
+df_men['weight_reg']=(df_men['nb_int']/df_men['pop_reg']*100000)
+df_men['weight_reg']=df_men['weight_reg'].fillna(0.0).astype(int)
+
+nb_men=df_men_age1["nb_int"].sum()
+df_men['weight_fr']=df_men['nb_int']/nb_men*1000000
+df_men['weight_fr']=df_men['weight_fr'].fillna(0.0).astype(int)
+
+df_men.to_csv('F:/PEPS/Données simulées/data_ref/geo_men.csv',index=False,columns= ['codgeo','dep','reg','weight_dep','weight_reg','weight_fr'])
+
+
+
+# calcul de la proportion de la pop men par age dep, reg 
+df_age_m2=pd.merge(df_age_m1,dep_men,on='dep',how='left')
+df_age_m=pd.merge(df_age_m2,reg_men,on='reg',how='left')
+
+df_age_m['weight_dep']=(df_age_m['nb_int']/df_men['pop_dep']*1000)+1
+df_age_m['weight_dep']=df_age_m['weight_dep'].fillna(0.0).astype(int)
+
+
+df_age_m['weight_reg']=(df_age_m['nb_int']/df_men['pop_reg']*10000)+1
+df_age_m['weight_reg']=df_age_m['weight_reg'].fillna(0.0).astype(int)
+
+df_age_m_reg['weight_reg']=(df_age_m_reg['nb_int']/df_men['pop_reg']*10000)+1
+df_age_m_reg['weight_reg']=df_age_m_reg['weight_reg'].fillna(0.0).astype(int)
+
+# calcul de la proportion de la pop men par age fr
+df_age_m_fr['weight_fr']=df_age_m_fr['nb_int']/nb_men*1000+1
+df_age_m_fr['weight_fr']=df_age_m_fr['weight_fr'].fillna(0.0).astype(int)
+
+df_age_m.to_csv('F:/PEPS/Données simulées/data_ref/age_men.csv',index=False,columns= ['age','dep','reg','weight_dep','weight_reg'])
+df_age_m_reg.to_csv('F:/PEPS/Données simulées/data_ref/age_men_reg.csv',index=False,columns= ['age','reg','weight_reg'])
+
+df_age_m_fr.to_csv('F:/PEPS/Données simulées/data_ref/age_men_fr.csv',index=False,columns= ['age','weight_fr'])
+
+
+# calcul de la proportion de la pop men par age et par dep, reg ou fr
+df_men_age2=pd.merge(df_men_age1,dep_men,on='dep',how='left')
+df_men_age=pd.merge(df_men_age2,reg_men,on='reg',how='left')
+
+df_men_age['weight_dep']=(df_men_age['nb_int']/df_men_age['pop_dep']*100000)
+df_men_age['weight_dep']=df_men_age['weight_dep'].fillna(0.0).astype(int)
+
+
+df_men_age['weight_reg']=(df_men_age['nb_int']/df_men_age['pop_reg']*100000)
+df_men_age['weight_reg']=df_men_age['weight_reg'].fillna(0.0).astype(int)
+
+nb_men=df_men_age["nb_int"].sum()
+df_men_age['weight_fr']=df_men_age['nb_int']/nb_men*1000000
+df_men_age['weight_fr']=df_men_age['weight_fr'].fillna(0.0).astype(int)
+
+df_men_age.to_csv('F:/PEPS/Données simulées/data_ref/geo_men_age.csv',index=False,columns= ['codgeo','age','dep','reg','weight_dep','weight_reg','weight_fr'])
+
+
+
+# preparation fichier pop women
+# ###########################################################################
+df_women_age1=pop.loc[(pop['sexe']==2), ['codgeo','age' ,'nb_int','dep','reg'] ]
+
+# aggreation par commune (hors age)
+df_women1=df_women_age1.groupby(['codgeo','dep','reg'], as_index=False)["nb_int"].sum()
+# aggreation par age dep reg 
+df_age_w1=df_women_age1.groupby(['age','dep','reg'], as_index=False)["nb_int"].sum()
+# aggreation par age reg 
+df_age_w_reg=df_women_age1.groupby(['age','reg'], as_index=False)["nb_int"].sum()
+# aggreation par hors age
+df_age_w_fr=df_women_age1.groupby(['age'], as_index=False)["nb_int"].sum()
+
+
+# totaux geographique (dep, reg, fr) de la pop women
+dep_women=df_women_age1.groupby("dep", as_index=False)["nb_int"].sum()
+dep_women = dep_women.rename(columns = {'nb_int':'pop_dep'})
+
+reg_women=df_women_age1.groupby("reg", as_index=False)["nb_int"].sum()
+reg_women = reg_women.rename(columns = {'nb_int':'pop_reg'})
+
+# calcul de la proportion de la pop women par dep, reg ou fr
+df_women2=pd.merge(df_women1,dep_women,on='dep',how='left')
+df_women=pd.merge(df_women2,reg_women,on='reg',how='left')
+
+df_women['weight_dep']=(df_women['nb_int']/df_women['pop_dep']*100000)
+df_women['weight_dep']=df_women['weight_dep'].fillna(0.0).astype(int)
+
+
+df_women['weight_reg']=(df_women['nb_int']/df_women['pop_reg']*100000)
+df_women['weight_reg']=df_women['weight_reg'].fillna(0.0).astype(int)
+
+nb_women=df_women_age1["nb_int"].sum()
+df_women['weight_fr']=df_women['nb_int']/nb_women*1000000
+df_women['weight_fr']=df_women['weight_fr'].fillna(0.0).astype(int)
+
+df_women.to_csv('F:/PEPS/Données simulées/data_ref/geo_women.csv',index=False,columns= ['codgeo','dep','reg','weight_dep','weight_reg','weight_fr'])
+
+
+
+# calcul de la proportion de la pop women par age dep, reg 
+df_age_w2=pd.merge(df_age_w1,dep_women,on='dep',how='left')
+df_age_w=pd.merge(df_age_w2,reg_women,on='reg',how='left')
+
+df_age_w['weight_dep']=(df_age_w['nb_int']/df_women['pop_dep']*1000)+1
+df_age_w['weight_dep']=df_age_w['weight_dep'].fillna(0.0).astype(int)
+
+
+df_age_w['weight_reg']=(df_age_w['nb_int']/df_women['pop_reg']*10000)+1
+df_age_w['weight_reg']=df_age_w['weight_reg'].fillna(0.0).astype(int)
+
+df_age_w_reg['weight_reg']=(df_age_w['nb_int']/df_women['pop_reg']*10000)+1
+df_age_w_reg['weight_reg']=df_age_w['weight_reg'].fillna(0.0).astype(int)
+
+
+# calcul de la proportion de la pop women par age fr
+df_age_w_fr['weight_fr']=df_age_w_fr['nb_int']/nb_women*1000+1
+df_age_w_fr['weight_fr']=df_age_w_fr['weight_fr'].fillna(0.0).astype(int)
+
+df_age_w.to_csv('F:/PEPS/Données simulées/data_ref/age_women.csv',index=False,columns= ['age','dep','reg','weight_dep','weight_reg'])
+df_age_w_reg.to_csv('F:/PEPS/Données simulées/data_ref/age_women_reg.csv',index=False,columns= ['age','reg','weight_reg'])
+df_age_w_fr.to_csv('F:/PEPS/Données simulées/data_ref/age_women_fr.csv',index=False,columns= ['age','weight_fr'])
+
+
+# calcul de la proportion de la pop women par age et par dep, reg ou fr
+df_women_age2=pd.merge(df_women_age1,dep_women,on='dep',how='left')
+df_women_age=pd.merge(df_women_age2,reg_women,on='reg',how='left')
+
+df_women_age['weight_dep']=(df_women_age['nb_int']/df_women_age['pop_dep']*100000)
+df_women_age['weight_dep']=df_women_age['weight_dep'].fillna(0.0).astype(int)
+
+
+df_women_age['weight_reg']=(df_women_age['nb_int']/df_women_age['pop_reg']*100000)
+df_women_age['weight_reg']=df_women_age['weight_reg'].fillna(0.0).astype(int)
+
+nb_women=df_women_age["nb_int"].sum()
+df_women_age['weight_fr']=df_women_age['nb_int']/nb_women*1000000
+df_women_age['weight_fr']=df_women_age['weight_fr'].fillna(0.0).astype(int)
+
+df_women_age.to_csv('F:/PEPS/Données simulées/data_ref/geo_women_age.csv',index=False,columns= ['codgeo','age','dep','reg','weight_dep','weight_reg','weight_fr'])
---
+++ drive_cp_line.py
@@ -0,0 +1,365 @@
+# generation automatique de fichiers
+
+import pandas as pd
+import json
+import datetime
+import random
+#import numpy.random as rd
+from scipy.stats import truncnorm
+
+# ################################################################################
+# fonctions
+# ################################################################################
+
+def gen_yob (age_mean,age_sd,ref_year):
+     # attribution aleatoire de l'annee de naissance selon la fourchette des parametres
+    #yob=ref_year-(random.randint(age_mean-age_sd,age_mean+age_sd))
+    # yob=ref_year-int((random.gauss(age_mean, age_sd)))
+    y=truncnorm((30 - age_mean) / age_sd, (90 - age_mean) / age_sd, loc=age_mean, scale=age_sd)
+    year=y.rvs()
+    yob=ref_year-int(year)
+    
+    return yob
+
+
+def gen_mob ():
+     # attribution aleatoire du mois de naissance
+    l_month =['01','02','03','04','05','06','07','08','09','10','11','12']
+    m = random.randint(0, len(l_month) - 1)  
+    mob = l_month[m]  
+    
+    return mob
+
+
+def gen_com():
+    # attribution aleatoire ponderee (cf. recensement (weight)) de la commune de residence
+    # selon le niveau geo 
+    com = random.choices(l_depcom,l_comweight)[0]
+ 
+    return com
+
+
+  
+def gen_events_init_lin (init_dte,occ,yn_occ,delay_mean,mark):
+
+    if yn_occ==1:
+    # generation de la liste des dates d'evt possibles
+        if occ==1:
+            end_dte=min(init_dte+datetime.timedelta(days=delay_mean),fin)
+            l_evt_dte = pd.date_range(init_dte, end_dte).tolist()
+        else:
+            begin_dte=max(init_dte-datetime.timedelta(days=delay_mean),deb)
+            l_evt_dte = pd.date_range(begin_dte,init_dte).tolist()
+                       
+        evt_dte = random.choice(l_evt_dte)
+    else:
+        evt_dte=None
+       
+    new_evt=[mark,evt_dte]
+        
+
+    return new_evt
+
+
+ 
+def gen_events_lin (yn_occ,mark):
+
+    if yn_occ==1:
+    # generation de la liste des dates d'evt possibles
+        l_evt_dte = pd.date_range(deb, fin).tolist()                     
+        evt_dte = random.choice(l_evt_dte)
+    else:
+        evt_dte=None
+       
+    new_evt=[mark,evt_dte]
+        
+
+    return new_evt
+
+
+
+    
+# generation d'un nouveau patient 
+def gen_new_pat_init (num_pat,sex,age_mean,age_sd,ref_year,delay_death):
+    
+    # tirage au sort l'annee de naissance
+    yob=gen_yob(age_mean,age_sd,ref_year)
+    # tirage au sort du mois de naissance
+    mob=gen_mob()
+    # tirage au sort de la commune
+    com=gen_com()    
+    # tirage au sort de la date de l'event initial
+    init_dte = random.choice(l_date)
+    # Date de deces
+    death=y_n_death[0]
+    # tirage au sort de la date de dc
+    if death==1:
+        # generation de la liste des dates de dc possibles 
+        end_dte=min(init_dte+datetime.timedelta(days=delay_death),fin)
+        l_death_dte = pd.date_range(init_dte, end_dte).tolist()
+        death_dte = random.choice(l_death_dte)
+    else:
+        death_dte=None
+
+    #pat=[num_pat,sex,yob,mob,com,init_dte,death,death_dte]
+    #new_pat=[num_pat,sex,yob,mob,com,init_dte,death,death_dte]
+    
+    # generation des events
+    evt_pat=[]
+    new_pat=[]
+    for e, evt in enumerate(l_event):
+        if l_y_n_init[e][0]==1:
+            for o in range (random.randint(evt['nb_occ'], evt['nb_occ_max'])):
+                new_evt=gen_events_init_lin(init_dte,evt['occ'],l_y_n_init[e][0],evt['delay_mean'],evt['mark'])
+                new_pat= [num_pat,sex,yob,mob,com,init_dte,death,death_dte]+new_evt
+                evt_pat.append(new_pat)
+            
+        del l_y_n_init[e][0]
+
+    new_pat= [num_pat,sex,yob,mob,com,init_dte,death,death_dte,l_init['mark'],init_dte]
+    evt_pat.append(new_pat)
+    return evt_pat
+
+
+# generation d'un nouveau patient 
+def gen_new_pat (num_pat,sex,age_mean,age_sd,ref_year):
+    
+    # tirage au sort l'annee de naissance
+    yob=gen_yob(age_mean,age_sd,ref_year)
+    # tirage au sort du mois de naissance
+    mob=gen_mob()
+    # tirage au sort de la commune
+    com=gen_com()    
+
+    # tirage au sort de la date de l'event initial
+    init_dte=None
+    
+    # Date de deces : selon proba
+    death=y_n_death[0]
+    # tirage au sort de la date de dc
+    if death==1:
+        # generation de la liste des dates de dc possibles 
+        l_death_dte = pd.date_range(deb, fin).tolist()
+        death_dte = random.choice(l_death_dte)
+    else:
+        death_dte=None
+
+   # pat=[num_pat,sex,yob,mob,com,init_dte,death,death_dte]
+   # new_pat=[num_pat,sex,yob,mob,com,init_dte,death,death_dte]
+    
+    # generation des events
+    evt_pat=[]
+    new_pat=[]
+    for e,evt in enumerate(l_event):
+        if l_y_n[e][0]==1:
+            for o in range (random.randint(evt['nb_occ'], evt['nb_occ_max'])):
+                new_evt=gen_events_lin(l_y_n[e][0],evt['mark'])
+                new_pat= [num_pat,sex,yob,mob,com,init_dte,death,death_dte]+new_evt
+                evt_pat.append(new_pat) 
+        
+        del l_y_n[e][0]
+
+    new_pat= [num_pat,sex,yob,mob,com,init_dte,death,death_dte,'INIT',init_dte]
+    evt_pat.append(new_pat)         
+    return evt_pat
+
+
+
+# ################################################################################
+# debut programme
+# ################################################################################
+
+# lecture du fichiers des parametres generaux
+f_param = json.load(open("param_cp.json"))
+param=f_param['parameters']
+l_init=f_param['init']
+l_event=f_param['events']
+
+
+# recuperation des parametres demographique
+nb_men_init=int(param['pop']*param['pct_men']*param['pct_ill'])
+nb_men=int(param['pop']*param['pct_men'])-nb_men_init
+
+nb_women_init=int(param['pop']*param['pct_ill']) - nb_men_init
+nb_women=param['pop']-int(param['pop']*param['pct_ill']) - nb_men
+
+deb= datetime.datetime.strptime(param['begin_dte'],'%Y-%m-%d') 
+fin=datetime.datetime.strptime(param['end_dte'],'%Y-%m-%d') 
+
+
+delay_death_m=l_init['delay_death_m']*365
+delay_death_w=l_init['delay_death_w']*365
+
+# generation de la liste des dates de la periode d'etude
+l_date = pd.date_range(param['begin_dte'], param['end_dte']).tolist()
+
+
+# constitution du dataframe geo correspondant aux paramètres choisis
+# tirage au sort de la commune au niveau de la Fr, d'une région ou d'un departement
+if param['geo_level']=='fr':
+    header_row = ['DEPCOM', 'DEP', 'REG', 'PTOT', 'weight', 'weight_reg', 'weight_dep']
+    df=pd.read_csv(filepath_or_buffer='data_ref/communes_fr.csv',sep=';', header=0, names=header_row)
+    df_com=df
+else:
+    if param['geo_level']=='reg':
+        header_row = ['DEPCOM', 'DEP', 'REG', 'PTOT', 'weight_fr', 'weight', 'weight_dep']
+        df=pd.read_csv(filepath_or_buffer='data_ref/communes_fr.csv',sep=';',header=0, names=header_row)
+        df_com=df[(df['REG']==param['geo_sel'])]
+    else:
+        if param['geo_level']=='dep':
+            header_row = ['DEPCOM', 'DEP', 'REG', 'PTOT', 'weight_fr', 'weight_reg', 'weight']
+            df=pd.read_csv(filepath_or_buffer='data_ref/communes_fr.csv',sep=';', header=0, names=header_row)
+            df_com=df[(df['DEP']==param['geo_sel'])]
+            
+
+df_com=df_com.reset_index(drop=True)
+
+l_depcom=df_com["DEPCOM"]
+l_comweight=df_com["weight"]
+
+
+# liste 0/1 (pct de patient malades) par evt pour les patients "init"
+# permet de maitriser le nb exact d'evt et donc respecter les parametres  
+l_y_n_init=[]
+for e in l_event:
+    y_n = [0]*((nb_men_init+nb_women_init)-int(e['pct_occ']*(nb_men_init+nb_women_init))) + [1]*int(e['pct_occ']*(nb_men_init+nb_women_init))
+    random.shuffle(y_n)
+    l_y_n_init.append(y_n)
+
+# liste 0/1 (pct de patient non malades) par evt pour les patients "sains"
+l_y_n=[]
+for e in l_event:
+    y_n = [0]*((nb_men+nb_women)-int(e['pct_occ_pop']*(nb_men+nb_women))) + [1]*int(e['pct_occ_pop']*(nb_men+nb_women))
+    random.shuffle(y_n)
+    l_y_n.append(y_n)
+    
+# ################################################################################
+# generation fichier patients
+# ################################################################################
+
+
+
+# generation de la population des hommes malades
+l_men_init=[]
+y_n_death = [0]*(nb_men_init-int(l_init['pct_death_m']*nb_men_init)) + [1]*int(l_init['pct_death_m']*nb_men_init)
+random.shuffle(y_n_death)
+
+
+for i in range(nb_men_init):
+    new_pat_init=gen_new_pat_init(i+1,1,l_init['age_mean_m'],l_init['age_sd_m'],param['ref_year'],delay_death_m)
+
+    for e,evt in enumerate(new_pat_init):
+        l_men_init.append(new_pat_init[e])
+        
+       
+    del y_n_death[0]
+
+
+# generation de la population des femmes malades 
+l_women_init=[]
+y_n_death = [0]*(nb_women_init-int(l_init['pct_death_w']*nb_women_init)) + [1]*int(l_init['pct_death_w']*nb_women_init)
+random.shuffle(y_n_death)
+for j in range(nb_men_init,int(param['pop']*param['pct_ill'])):
+    new_pat_init=gen_new_pat_init(j+1,2,l_init['age_mean_w'],l_init['age_sd_w'],param['ref_year'],delay_death_m)
+
+    for e,evt in enumerate(new_pat_init):
+        l_women_init.append(new_pat_init[e])
+
+    del y_n_death[0]
+    
+# generation de la population des hommes sans la maladie initiale
+l_men=[]
+y_n_death = [0]*(nb_men-int(param['pct_death_m_pop']*nb_men)) + [1]*int(param['pct_death_m_pop']*nb_men)
+random.shuffle(y_n_death)
+for k in range(nb_women_init+nb_men_init,nb_men+int(param['pop']*param['pct_ill'])):
+    new_pat=gen_new_pat(k+1,1,int(param['age_mean_m']),int(param['age_sd_m']),param['ref_year'])
+
+    for e,evt in enumerate(new_pat):  
+        l_men.append(new_pat[e])
+        
+    del y_n_death[0]
+
+# generation de la population des femmes sans la maladie initiale 
+l_women=[]
+y_n_death = [0]*(nb_women-int(param['pct_death_w_pop']*nb_women)) + [1]*int(param['pct_death_w_pop']*nb_women)
+random.shuffle(y_n_death)
+for l in range(nb_women_init+nb_men_init+nb_men,param['pop']):
+    new_pat=gen_new_pat(l+1,2,param['age_mean_w'],param['age_sd_w'],param['ref_year'])
+        
+    for e,evt in enumerate(new_pat): 
+        l_women.append(new_pat[e])
+        
+    del y_n_death[0]
+
+
+
+# preparation export csv, selon format
+col_evt=[]
+
+col_exp= ['num_pat', 'sex','yob','mob','com',l_init["mark"],'death','death_dte', 'mark','mark_dte']  
+col_exp_pat= ['num_pat', 'sex','yob','mob','com',l_init["mark"],'death','death_dte']  
+col_exp_evt= ['num_pat', 'mark','mark_dte']  
+
+
+df_all = pd.DataFrame(data=l_men_init+l_women_init+l_men+l_women,columns =col_exp)  
+df_pat_all=df_all.drop(['mark','mark_dte'] ,axis=1)
+df_pat_all=df_pat_all.drop_duplicates()
+
+
+df_evt_all=df_all.dropna(subset=['mark_dte'])
+df_evt_all=df_evt_all.drop(['sex','yob','mob','com',l_init["mark"],'death','death_dte'] ,axis=1)
+df_all=pd.merge(df_pat_all,df_evt_all,on='num_pat',how='left')
+df_all=df_all.drop(l_init["mark"] ,axis=1)    
+
+
+df_all["death_ts"] = (pd.to_datetime(df_all["death_dte"], format="%Y-%m-%d")-pd.to_datetime('1900-01-01', format="%Y-%m-%d")).dt.days
+df_all["mark_ts"] = (pd.to_datetime(df_all["mark_dte"], format="%Y-%m-%d")-pd.to_datetime('1900-01-01', format="%Y-%m-%d")).dt.days
+df_all["birth_ts"] = (pd.to_datetime(df_all["yob"].map(str)+df_all["mob"].map(str)+'01', format="%Y%m%d")-pd.to_datetime('1900-01-01', format="%Y-%m-%d")).dt.days
+
+
+
+
+df_init = pd.DataFrame(data=l_men_init+l_women_init,columns =col_exp)   
+df_pat_init=df_init.drop(['mark','mark_dte'] ,axis=1)
+df_pat_init=df_pat_init.drop_duplicates()
+
+df_evt_init=df_init.dropna(subset=['mark_dte'])
+df_evt_init=df_evt_init.drop(['sex','yob','mob','com',l_init["mark"],'death','death_dte'] ,axis=1)
+df_init=pd.merge(df_pat_init,df_evt_init,on='num_pat',how='left')
+df_init=df_init.drop(l_init["mark"] ,axis=1)    
+
+df_init["death_ts"] = (pd.to_datetime(df_init["death_dte"], format="%Y-%m-%d")-pd.to_datetime('1900-01-01', format="%Y-%m-%d")).dt.days
+df_init["mark_ts"] = (pd.to_datetime(df_init["mark_dte"], format="%Y-%m-%d")-pd.to_datetime('1900-01-01', format="%Y-%m-%d")).dt.days
+df_init["birth_ts"] = (pd.to_datetime(df_init["yob"].map(str)+df_init["mob"].map(str)+'01', format="%Y%m%d")-pd.to_datetime('1900-01-01', format="%Y-%m-%d")).dt.days
+
+
+
+df_healthy = pd.DataFrame(data=l_men+l_women,columns =col_exp)
+df_pat_healthy=df_healthy.drop(['mark','mark_dte'] ,axis=1)
+df_pat_healthy=df_pat_healthy.drop_duplicates()
+
+df_evt_healthy=df_healthy.dropna(subset=['mark_dte'])
+df_evt_healthy=df_evt_healthy.drop(['sex','yob','mob','com',l_init["mark"],'death','death_dte'] ,axis=1)
+df_healthy=pd.merge(df_pat_healthy,df_evt_healthy,on='num_pat',how='left')
+df_healthy=df_healthy.drop(l_init["mark"] ,axis=1)    
+
+
+df_healthy["death_ts"] = (pd.to_datetime(df_healthy["death_dte"], format="%Y-%m-%d")-pd.to_datetime('1900-01-01', format="%Y-%m-%d")).dt.days
+df_healthy["mark_ts"] = (pd.to_datetime(df_healthy["mark_dte"], format="%Y-%m-%d")-pd.to_datetime('1900-01-01', format="%Y-%m-%d")).dt.days
+df_healthy["birth_ts"] = (pd.to_datetime(df_healthy["yob"].map(str)+df_healthy["mob"].map(str)+'01', format="%Y%m%d")-pd.to_datetime('1900-01-01', format="%Y-%m-%d")).dt.days
+
+
+
+
+
+# export de la population dans un fichier csv
+df_all.to_csv('files/file_all.csv',index=False,sep=';')
+
+df_init.to_csv('files/file_init.csv',index=False,sep=';')
+
+df_healthy.to_csv('files/file_healthy.csv',index=False,sep=';')
+
+
+
+
+
