ef35df0871ff8436219a10cd5e63d6da59896629
cubr <contact@cubr.fr>
2020-04-17 17:29:23 -0400

    cosmit

--- drive_cp_line.py
+++ drive_cp_line.py
@@ -5,12 +5,45 @@ import json
 import datetime
 import random
 import sys
-#import numpy.random as rd
+import os
+import shutil
+from pathlib import Path
+import numpy as np
 from scipy.stats import truncnorm
 
-# ################################################################################
+#################################################################################
+comprefix="0"  # prefix to be added to 'commune' variable
+
+timeorigin = pd.to_datetime('1900-01-01', format="%Y-%m-%d");
+
+#################################################################################
+# progress bar (https://stackoverflow.com/questions/3173320/text-progress-bar-in-the-console)
+#################################################################################
+# Print iterations progress
+def printProgressBar (iteration, total, prefix = '', suffix = '', decimals = 1, length = 100, fill = '█', printEnd = "\r"):
+    """
+    Call in a loop to create terminal progress bar
+    @params:
+        iteration   - Required  : current iteration (Int)
+        total       - Required  : total iterations (Int)
+        prefix      - Optional  : prefix string (Str)
+        suffix      - Optional  : suffix string (Str)
+        decimals    - Optional  : positive number of decimals in percent complete (Int)
+        length      - Optional  : character length of bar (Int)
+        fill        - Optional  : bar fill character (Str)
+        printEnd    - Optional  : end character (e.g. "\r", "\r\n") (Str)
+    """
+    percent = ("{0:." + str(decimals) + "f}").format(100 * (iteration / float(total)))
+    filledLength = int(length * iteration // total)
+    bar = fill * filledLength + '-' * (length - filledLength)
+    print('\r%s |%s| %s%% %s' % (prefix, bar, percent, suffix), end = printEnd)
+    # Print New Line on Complete
+    if iteration == total: 
+        print()
+
+#################################################################################
 # fonctions
-# ################################################################################
+#################################################################################
 
 def gen_yob (age_mean,age_sd,ref_year):
      # attribution aleatoire de l'annee de naissance selon la fourchette des parametres
@@ -22,7 +55,7 @@ def gen_yob (age_mean,age_sd,ref_year):
     
     return yob
 
-
+################################################################################
 def gen_mob ():
      # attribution aleatoire du mois de naissance
     l_month =['01','02','03','04','05','06','07','08','09','10','11','12']
@@ -31,16 +64,17 @@ def gen_mob ():
     
     return mob
 
-
+################################################################################
 def gen_com():
     # attribution aleatoire ponderee (cf. recensement (weight)) de la commune de residence
-    # selon le niveau geo 
+    # selon le niveau geo
     com = random.choices(l_depcom,l_comweight)[0]
  
+    com = comprefix + com;
+    
     return com
 
-
-  
+# ################################################################################
 def gen_events_init_lin (init_dte,occ,yn_occ,delay_mean,mark,end_dte):
     l_evt_dte=[]
     if yn_occ==1:
@@ -57,12 +91,10 @@ def gen_events_init_lin (init_dte,occ,yn_occ,delay_mean,mark,end_dte):
         evt_dte=None
        
     new_evt=[mark,evt_dte]
-        
 
     return new_evt
 
-
- 
+# ################################################################################
 def gen_events_lin (yn_occ,mark,end_dte):
 
     if yn_occ==1:
@@ -74,12 +106,9 @@ def gen_events_lin (yn_occ,mark,end_dte):
        
     new_evt=[mark,evt_dte]
         
-
     return new_evt
 
-
-
-    
+################################################################################
 # generation d'un nouveau patient malade
 def gen_new_pat_init (num_pat,sex,age_mean,age_sd,ref_year,delay_death):
     
@@ -120,7 +149,7 @@ def gen_new_pat_init (num_pat,sex,age_mean,age_sd,ref_year,delay_death):
     evt_pat.append(new_pat)
     return evt_pat
 
-
+################################################################################
 # generation d'un nouveau patient non malade
 def gen_new_pat (num_pat,sex,age_mean,age_sd,ref_year):
     
@@ -162,41 +191,61 @@ def gen_new_pat (num_pat,sex,age_mean,age_sd,ref_year):
     evt_pat.append(new_pat)         
     return evt_pat
 
-
-
-# ################################################################################
+#################################################################################
 # debut programme
-# ################################################################################
+#################################################################################
 
 if len(sys.argv)<1:
     print ("You must provide :")
     print ("   1) [IN]  a json parameter file")
     sys.exit();
 
-if len(sys.argv)==0:
-    file_param   = sys.argv[1]
-else:
-    file_param='param_cp.json'
+file_param = sys.argv[1];
+
 # lecture du fichiers des parametres generaux
 f_param = json.load(open(file_param))
-param=f_param['parameters']
-l_init=f_param['init']
-l_event=f_param['events']
 
+param   = f_param['parameters']
+l_init  = f_param['init']
+l_event = f_param['events']
+
+# if no output directory is defined, we create one in the current folder
+if not "rep_file" in param: 
+    base = Path(file_param).stem;
+    outputDir = "run_" + base + "_" + datetime.datetime.now().strftime("%Y%m%d_%H%M%S")
+    print ("no 'rep_file' defined -> creating directory", outputDir);
+    os.mkdir (outputDir); 
+    param["rep_file"] = outputDir + "/"
+
+if not param["rep_file"].endswith("/"):  param["rep_file"] += "/"
+
+# we also copy the configuration file in the output directory
+shutil.copy(file_param, param["rep_file"] + os.path.basename(file_param));
+
+file_all     = param.get("file_all",     "file_all.csv")
+file_init    = param.get("file_init",    "file_init.csv")
+file_healthy = param.get("file_healthy", "file_healthy.csv")
+
+# we initialize the random generator by a seed (if any provided by the user)
+# by default, a 0 seed is used which ensures that the outputs are the same for the same config.
+seed = param.get("seed", "0")
+random.seed (int(seed));
+np.random.seed (int(seed));
+
+if 'first_num_pat' not in param:  param['first_num_pat'] = 0;
 
 # recuperation des parametres demographique
-nb_men_init=int(param['pop']*param['pct_men']*param['pct_ill'])
-nb_men=int(param['pop']*param['pct_men'])-nb_men_init
+nb_men_init   = int(param['pop']*param['pct_men']*param['pct_ill'])
+nb_men        = int(param['pop']*param['pct_men'])-nb_men_init
 
-nb_women_init=int(param['pop']*param['pct_ill']) - nb_men_init
-nb_women=param['pop']-int(param['pop']*param['pct_ill']) - nb_men
+nb_women_init = int(param['pop']*param['pct_ill']) - nb_men_init
+nb_women      = param['pop']-int(param['pop']*param['pct_ill']) - nb_men
 
-deb= datetime.datetime.strptime(param['begin_dte'],'%Y-%m-%d') 
-fin=datetime.datetime.strptime(param['end_dte'],'%Y-%m-%d') 
+deb = datetime.datetime.strptime(param['begin_dte'],'%Y-%m-%d') 
+fin = datetime.datetime.strptime(param['end_dte'],  '%Y-%m-%d') 
 
-
-delay_death_m=l_init['delay_death_m']*365
-delay_death_w=l_init['delay_death_w']*365
+delay_death_m = l_init['delay_death_m']*365
+delay_death_w = l_init['delay_death_w']*365
 
 # generation de la liste des dates de la periode d'etude
 l_date = pd.date_range(param['begin_dte'], param['end_dte']).tolist()
@@ -208,23 +257,21 @@ if param['geo_level']=='fr':
     header_row = ['DEPCOM', 'DEP', 'REG', 'PTOT', 'weight', 'weight_reg', 'weight_dep']
     df=pd.read_csv(filepath_or_buffer='data_ref/communes_fr.csv',sep=';', header=0, names=header_row)
     df_com=df
-else:
-    if param['geo_level']=='reg':
-        header_row = ['DEPCOM', 'DEP', 'REG', 'PTOT', 'weight_fr', 'weight', 'weight_dep']
-        df=pd.read_csv(filepath_or_buffer='data_ref/communes_fr.csv',sep=';',header=0, names=header_row)
-        df_com=df[(df['REG']==param['geo_sel'])]
-    else:
-        if param['geo_level']=='dep':
-            header_row = ['DEPCOM', 'DEP', 'REG', 'PTOT', 'weight_fr', 'weight_reg', 'weight']
-            df=pd.read_csv(filepath_or_buffer='data_ref/communes_fr.csv',sep=';', header=0, names=header_row)
-            df_com=df[(df['DEP']==param['geo_sel'])]
-            
 
-df_com=df_com.reset_index(drop=True)
+elif param['geo_level']=='reg':
+    header_row = ['DEPCOM', 'DEP', 'REG', 'PTOT', 'weight_fr', 'weight', 'weight_dep']
+    df=pd.read_csv(filepath_or_buffer='data_ref/communes_fr.csv',sep=';',header=0, names=header_row)
+    df_com=df[(df['REG']==param['geo_sel'])]
 
-l_depcom=df_com["DEPCOM"]
-l_comweight=df_com["weight"]
+elif param['geo_level']=='dep':
+    header_row = ['DEPCOM', 'DEP', 'REG', 'PTOT', 'weight_fr', 'weight_reg', 'weight']
+    df=pd.read_csv(filepath_or_buffer='data_ref/communes_fr.csv',sep=';', header=0, names=header_row)
+    df_com=df[(df['DEP']==param['geo_sel'])]
 
+df_com = df_com.reset_index(drop=True)
+
+l_depcom    = df_com["DEPCOM"]
+l_comweight = df_com["weight"]
 
 # liste 0/1 (pct de patient malades) par evt pour les patients "init"
 # permet de maitriser le nb exact d'evt et donc respecter les parametres  
@@ -245,28 +292,27 @@ for e in l_event:
 # generation fichier patients
 # ################################################################################
 
-
 # generation de la population des hommes malades
 l_men_init=[]
 y_n_death = [0]*(nb_men_init-int(l_init['pct_death_m']*nb_men_init)) + [1]*int(l_init['pct_death_m']*nb_men_init)
 random.shuffle(y_n_death)
-
 
 for i in range(nb_men_init):
     new_pat_init=gen_new_pat_init(i+1,1,l_init['age_mean_m'],l_init['age_sd_m'],param['ref_year'],delay_death_m)
 
     for e,evt in enumerate(new_pat_init):
         l_men_init.append(new_pat_init[e])
-        
        
     del y_n_death[0]
-
+    
+    printProgressBar(i, nb_men_init-1, prefix="men   (ill)", length=50)
 
 # generation de la population des femmes malades 
 l_women_init=[]
 y_n_death = [0]*(nb_women_init-int(l_init['pct_death_w']*nb_women_init)) + [1]*int(l_init['pct_death_w']*nb_women_init)
 random.shuffle(y_n_death)
-for j in range(nb_men_init,int(param['pop']*param['pct_ill'])):
+[i0,i1] = [nb_men_init,int(param['pop']*param['pct_ill'])]
+for j in range(i0,i1):
     new_pat_init=gen_new_pat_init(j+1,2,l_init['age_mean_w'],l_init['age_sd_w'],param['ref_year'],delay_death_m)
 
     for e,evt in enumerate(new_pat_init):
@@ -274,23 +320,29 @@ for j in range(nb_men_init,int(param['pop']*param['pct_ill'])):
 
     del y_n_death[0]
     
+    printProgressBar(j-i0, i1-i0-1, prefix="women (ill)", length=50)
+
 # generation de la population des hommes sans la maladie initiale
 l_men=[]
 y_n_death = [0]*(nb_men-int(param['pct_death_m_pop']*nb_men)) + [1]*int(param['pct_death_m_pop']*nb_men)
 random.shuffle(y_n_death)
-for k in range(nb_women_init+nb_men_init,nb_men+int(param['pop']*param['pct_ill'])):
-    new_pat=gen_new_pat(k+1,1,int(param['age_mean_m']),int(param['age_sd_m']),param['ref_year'])
+[i0,i1] = [nb_women_init+nb_men_init, nb_men+int(param['pop']*param['pct_ill'])]
+for k in range(i0,i1):
+    new_pat = gen_new_pat(k+1,1,int(param['age_mean_m']),int(param['age_sd_m']),param['ref_year'])
 
     for e,evt in enumerate(new_pat):  
         l_men.append(new_pat[e])
         
     del y_n_death[0]
 
+    printProgressBar(k-i0, i1-i0-1, prefix="men        ", length=50)
+
 # generation de la population des femmes sans la maladie initiale 
 l_women=[]
 y_n_death = [0]*(nb_women-int(param['pct_death_w_pop']*nb_women)) + [1]*int(param['pct_death_w_pop']*nb_women)
 random.shuffle(y_n_death)
-for l in range(nb_women_init+nb_men_init+nb_men,param['pop']):
+[i0,i1] = [nb_women_init+nb_men_init+nb_men, param['pop']]
+for l in range(i0,i1):
     new_pat=gen_new_pat(l+1,2,param['age_mean_w'],param['age_sd_w'],param['ref_year'])
         
     for e,evt in enumerate(new_pat): 
@@ -298,73 +350,55 @@ for l in range(nb_women_init+nb_men_init+nb_men,param['pop']):
         
     del y_n_death[0]
 
-
+    printProgressBar(l-i0, i1-i0-1, prefix="women      ", length=50)
 
 # preparation export csv, selon format
 col_evt=[]
 
-col_exp= ['num_pat', 'sex','yob','mob','com',l_init["mark"],'death','death_dte', 'mark','mark_dte']  
+col_exp    = ['num_pat', 'sex','yob','mob','com',l_init["mark"],'death','death_dte', 'mark','mark_dte']  
 col_exp_pat= ['num_pat', 'sex','yob','mob','com',l_init["mark"],'death','death_dte']  
 col_exp_evt= ['num_pat', 'mark','mark_dte']  
 
+df_all     = pd.DataFrame(data=l_men_init+l_women_init+l_men+l_women,columns =col_exp)  
+df_pat_all = df_all.drop(['mark','mark_dte'] ,axis=1)
+df_pat_all = df_pat_all.drop_duplicates()
 
-df_all = pd.DataFrame(data=l_men_init+l_women_init+l_men+l_women,columns =col_exp)  
-df_pat_all=df_all.drop(['mark','mark_dte'] ,axis=1)
-df_pat_all=df_pat_all.drop_duplicates()
-
-
-df_evt_all=df_all.dropna(subset=['mark_dte'])
-df_evt_all=df_evt_all.drop(['sex','yob','mob','com',l_init["mark"],'death','death_dte'] ,axis=1)
-df_all=pd.merge(df_pat_all,df_evt_all,on='num_pat',how='left')
-df_all=df_all.drop(l_init["mark"] ,axis=1)    
-
+df_evt_all = df_all.dropna(subset=['mark_dte'])
+df_evt_all = df_evt_all.drop(['sex','yob','mob','com',l_init["mark"],'death','death_dte'] ,axis=1)
+df_all     = pd.merge(df_pat_all,df_evt_all,on='num_pat',how='left')
+df_all     = df_all.drop(l_init["mark"] ,axis=1)    
 
 df_all["death_ts"] = (pd.to_datetime(df_all["death_dte"], format="%Y-%m-%d")-pd.to_datetime('1900-01-01', format="%Y-%m-%d")).dt.days
-df_all["mark_ts"] = (pd.to_datetime(df_all["mark_dte"], format="%Y-%m-%d")-pd.to_datetime('1900-01-01', format="%Y-%m-%d")).dt.days
+df_all["mark_ts"]  = (pd.to_datetime(df_all["mark_dte"], format="%Y-%m-%d")-pd.to_datetime('1900-01-01', format="%Y-%m-%d")).dt.days
 df_all["birth_ts"] = (pd.to_datetime(df_all["yob"].map(str)+df_all["mob"].map(str)+'01', format="%Y%m%d")-pd.to_datetime('1900-01-01', format="%Y-%m-%d")).dt.days
 
+df_init     = pd.DataFrame(data=l_men_init+l_women_init,columns =col_exp)   
+df_pat_init = df_init.drop(['mark','mark_dte'] ,axis=1)
+df_pat_init = df_pat_init.drop_duplicates()
 
+df_evt_init = df_init.dropna(subset=['mark_dte'])
+df_evt_init = df_evt_init.drop(['sex','yob','mob','com',l_init["mark"],'death','death_dte'] ,axis=1)
+df_init     = pd.merge(df_pat_init,df_evt_init,on='num_pat',how='left')
+df_init     = df_init.drop(l_init["mark"] ,axis=1)    
 
+df_init["death_ts"] = (pd.to_datetime(df_init["death_dte"], format="%Y-%m-%d")-timeorigin).dt.days
+df_init["mark_ts"]  = (pd.to_datetime(df_init["mark_dte"],  format="%Y-%m-%d")-timeorigin).dt.days
+df_init["birth_ts"] = (pd.to_datetime(df_init["yob"].map(str)+df_init["mob"].map(str)+'01', format="%Y%m%d")-timeorigin).dt.days
 
-df_init = pd.DataFrame(data=l_men_init+l_women_init,columns =col_exp)   
-df_pat_init=df_init.drop(['mark','mark_dte'] ,axis=1)
-df_pat_init=df_pat_init.drop_duplicates()
+df_healthy     = pd.DataFrame(data=l_men+l_women,columns =col_exp)
+df_pat_healthy = df_healthy.drop(['mark','mark_dte'] ,axis=1)
+df_pat_healthy = df_pat_healthy.drop_duplicates()
 
-df_evt_init=df_init.dropna(subset=['mark_dte'])
-df_evt_init=df_evt_init.drop(['sex','yob','mob','com',l_init["mark"],'death','death_dte'] ,axis=1)
-df_init=pd.merge(df_pat_init,df_evt_init,on='num_pat',how='left')
-df_init=df_init.drop(l_init["mark"] ,axis=1)    
+df_evt_healthy = df_healthy.dropna(subset=['mark_dte'])
+df_evt_healthy = df_evt_healthy.drop(['sex','yob','mob','com',l_init["mark"],'death','death_dte'] ,axis=1)
+df_healthy     = pd.merge(df_pat_healthy,df_evt_healthy,on='num_pat',how='left')
+df_healthy     = df_healthy.drop(l_init["mark"] ,axis=1)    
 
-df_init["death_ts"] = (pd.to_datetime(df_init["death_dte"], format="%Y-%m-%d")-pd.to_datetime('1900-01-01', format="%Y-%m-%d")).dt.days
-df_init["mark_ts"] = (pd.to_datetime(df_init["mark_dte"], format="%Y-%m-%d")-pd.to_datetime('1900-01-01', format="%Y-%m-%d")).dt.days
-df_init["birth_ts"] = (pd.to_datetime(df_init["yob"].map(str)+df_init["mob"].map(str)+'01', format="%Y%m%d")-pd.to_datetime('1900-01-01', format="%Y-%m-%d")).dt.days
-
-
-
-df_healthy = pd.DataFrame(data=l_men+l_women,columns =col_exp)
-df_pat_healthy=df_healthy.drop(['mark','mark_dte'] ,axis=1)
-df_pat_healthy=df_pat_healthy.drop_duplicates()
-
-df_evt_healthy=df_healthy.dropna(subset=['mark_dte'])
-df_evt_healthy=df_evt_healthy.drop(['sex','yob','mob','com',l_init["mark"],'death','death_dte'] ,axis=1)
-df_healthy=pd.merge(df_pat_healthy,df_evt_healthy,on='num_pat',how='left')
-df_healthy=df_healthy.drop(l_init["mark"] ,axis=1)    
-
-
-df_healthy["death_ts"] = (pd.to_datetime(df_healthy["death_dte"], format="%Y-%m-%d")-pd.to_datetime('1900-01-01', format="%Y-%m-%d")).dt.days
-df_healthy["mark_ts"] = (pd.to_datetime(df_healthy["mark_dte"], format="%Y-%m-%d")-pd.to_datetime('1900-01-01', format="%Y-%m-%d")).dt.days
-df_healthy["birth_ts"] = (pd.to_datetime(df_healthy["yob"].map(str)+df_healthy["mob"].map(str)+'01', format="%Y%m%d")-pd.to_datetime('1900-01-01', format="%Y-%m-%d")).dt.days
-
-
-
-
+df_healthy["death_ts"] = (pd.to_datetime(df_healthy["death_dte"], format="%Y-%m-%d")-timeorigin).dt.days
+df_healthy["mark_ts"]  = (pd.to_datetime(df_healthy["mark_dte"],  format="%Y-%m-%d")-timeorigin).dt.days
+df_healthy["birth_ts"] = (pd.to_datetime(df_healthy["yob"].map(str)+df_healthy["mob"].map(str)+'01', format="%Y%m%d")-timeorigin).dt.days
 
 # export de la population dans un fichier csv
-df_all.to_csv(param["rep_file"]+'file_all.csv',index=False,sep=';')
-
-df_init.to_csv(param["rep_file"]+'file_init.csv',index=False,sep=';')
-
-df_healthy.to_csv(param["rep_file"]+'file_healthy.csv',index=False,sep=';')
-
-
-
+df_all.    to_csv (param["rep_file"]+ file_all,     index=False, sep=';')
+df_init.   to_csv (param["rep_file"]+ file_init ,   index=False, sep=';')
+df_healthy.to_csv (param["rep_file"]+ file_healthy, index=False, sep=';')
--- drive_errors_evt.py
+++ drive_errors_evt.py
@@ -7,82 +7,89 @@ import datetime
 import random
 #import numpy.random as rd
 
+f_errors    = json.load(open(sys.argv[1]))
 
-f_errors = json.load(open("param_errors.json"))
-l_scenarios=f_errors['scenarios']
-l_param=f_errors['parameters']
-pct_err=l_param["pct_err"]
-init_file=l_param["init_file"]
+l_scenarios = f_errors['scenarios']
+l_param     = f_errors['parameters']
+pct_err     = l_param ["pct_err"]
+init_file   = l_param ["init_file"]
 
-df_file=pd.read_csv(init_file, sep=';')
+# we read the file to be modified
+df_file = pd.read_csv(init_file, sep=';')
 
-
-df_init=df_file[(df_file['mark']==l_param['init'])]
+df_init = df_file[(df_file['mark']==l_param['init'])]
                             
-l_init=df_init.index.values.tolist()
-# nb erreurs calcule selon le nb d'evt
-nb_err=int(pct_err*len(df_file))
+l_init  = df_init.index.values.tolist()
 
+# nb erreurs calcule selon le nb d'evt
+nb_err = int(pct_err*len(df_file))
 
 # fixer la série "aleatoire" ?
 #random.seed(?)
 
-
 for s, scen in enumerate(l_scenarios):
     
-    nb_files=l_scenarios[s]["nb_files"]
-    l_events=l_scenarios[s]["evt"]
+    nb_files = l_scenarios[s]["nb_files"]
+    l_events = l_scenarios[s]["evt"]
+    
     for f in range(nb_files):
+        
         file_name=l_scenarios[s]["scenario"]+"_n"+str(f)+".csv"
         df_err=df_file.copy()
         err=0
+        
         for e,evt in enumerate(l_scenarios[s]["evt"]):
+            
             l_evt=random.sample(range(0,len(df_file)),nb_err)
             #l_init_sel=random.sample(range(0,len(l_init)),nb_err)
             l_init_sel=random.sample(l_init,nb_err)
             err=err+int(nb_err*evt['pct_err'])
+            
             for p in range(err):
+            
                 numpat=df_err['num_pat'][l_evt[p]]
+                
                 if evt['err']=='yob':
-                    if evt['typ_err']=='null':
-                        df_err.loc[df_err['num_pat']==numpat,'yob']=None
-                    if evt['typ_err']=='':
-                        df_err.loc[df_err['num_pat']==numpat,'yob']=2030
+                    if evt['typ_err']=='null':  df_err.loc[df_err['num_pat']==numpat,'yob'] = None
+                    if evt['typ_err']==''    :  df_err.loc[df_err['num_pat']==numpat,'yob'] = 2030
                         
                 if evt['err']=='mob':
-                    if evt['typ_err']=='null':
-                        df_err.loc[df_err['num_pat']==numpat,'mob']=None
-                    if evt['typ_err']=='':
-                        df_err.loc[df_err['num_pat']==numpat,'mob']='99'    
+                    if evt['typ_err']=='null':  df_err.loc[df_err['num_pat']==numpat,'mob'] = None
+                    if evt['typ_err']==''    :  df_err.loc[df_err['num_pat']==numpat,'mob'] = '99'    
                         
                 if evt['err']=='death':
+                    
                     if evt['typ_err']=='null':
-                        df_err.loc[df_err['num_pat']==numpat,'death_dte']=None
-                        df_err.loc[df_err['num_pat']==numpat,'death_ts']=None
+                        df_err.loc[df_err['num_pat']==numpat,'death_dte'] = None
+                        df_err.loc[df_err['num_pat']==numpat,'death_ts']  = None
+                    
                     if evt['typ_err']=='date':   
                         if pd.isnull(df_err['death_dte'][l_evt[p]]):
-                            df_err.loc[df_err['num_pat']==numpat,'death_dte']=pd.to_datetime('2030-01-01', format="%Y-%m-%d")
-                            df_err.loc[df_err['num_pat']==numpat,'death_ts']=47483
+                            df_err.loc[df_err['num_pat']==numpat,'death_dte'] = pd.to_datetime('2030-01-01', format="%Y-%m-%d")
+                            df_err.loc[df_err['num_pat']==numpat,'death_ts']  = 47483
                         else:
-                            df_err.loc[df_err['num_pat']==numpat,'death_dte']=pd.to_datetime(df_err['death_dte'][l_evt[p]])+datetime.timedelta(days=evt['err_sd'])
-                            df_err.loc[df_err['num_pat']==numpat,'death_ts']=df_err['death_ts'][l_evt[p]]+evt['err_sd']                          
+                            df_err.loc[df_err['num_pat']==numpat,'death_dte'] = pd.to_datetime(df_err['death_dte'][l_evt[p]])+datetime.timedelta(days=evt['err_sd'])
+                            df_err.loc[df_err['num_pat']==numpat,'death_ts']  = df_err['death_ts'][l_evt[p]]+evt['err_sd']                          
+
                 if evt['err']=='mark':
+                    
                     if evt['typ_err']=='null':
-                        df_err['mark_dte'][l_evt[p]]=None  
-                        df_err['mark_ts'][l_evt[p]]=None  
+                        df_err['mark_dte'][l_evt[p]] = None  
+                        df_err['mark_ts'] [l_evt[p]] = None  
+                    
                     if evt['typ_err']=='date':   
-                        df_err['mark_dte'][l_evt[p]]=pd.to_datetime(df_err['mark_dte'][l_evt[p]])+datetime.timedelta(days=evt['err_sd'])
-                        df_err['mark_ts'][l_evt[p]]=df_err['mark_ts'][l_evt[p]]+evt['err_sd']
+                        df_err['mark_dte'][l_evt[p]] = pd.to_datetime(df_err['mark_dte'][l_evt[p]])+datetime.timedelta(days=evt['err_sd'])
+                        df_err['mark_ts'] [l_evt[p]] = df_err['mark_ts'][l_evt[p]]+evt['err_sd']
+                    
                     if evt['typ_err']=='add': 
                         df_add=df_init.copy()
-                        df_add['num_pat']=10000000+p
-                        df_add['mark_dte']=pd.to_datetime(df_add['mark_dte'])+datetime.timedelta(days=evt['err_sd'])
-                        df_add['mark_ts']=df_add['mark_ts']+evt['err_sd']
+                        df_add['num_pat']  = 10000000+p
+                        df_add['mark_dte'] = pd.to_datetime(df_add['mark_dte'])+datetime.timedelta(days=evt['err_sd'])
+                        df_add['mark_ts']  = df_add['mark_ts']+evt['err_sd']
                         df_err=pd.DataFrame(df_add[df_add.index==[l_init_sel[p]]]).append(df_err, ignore_index=True) 
+                    
                     if evt['typ_err']=='del': 
-                        df_err=df_err.drop([l_init_sel[p]])
-
-                        
+                        df_err = df_err.drop([l_init_sel[p]])
 
         df_err.to_csv(l_param["rep_err"]+file_name,index=False,sep=';')                
 
